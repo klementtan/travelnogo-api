4e8991cedfe48ff05e95a62019097d3eafa89919
klementtan <klementtan@gmail.com>
2020-03-19 18:37:46 +0800

    Complete multiple update

--- app/controllers/api/v1/bans_controller.rb
+++ app/controllers/api/v1/bans_controller.rb
@@ -23,6 +23,37 @@ class Api::V1::BansController < Api::V1::BaseController
     }
   end
 
+  def create_many
+    authenticate
+    
+    banner = Country.find_by_code(params[:banner])
+
+    bans = []
+
+    params[:bannee].each { |bannee_code|
+      bannee = Country.find_by_code(bannee_code)
+      ban = Ban.find_by(banner: banner, bannee: bannee)
+      unless ban 
+        ban = Ban.create(:banner => banner, :bannee => bannee)
+      end
+      if params[:ban_type]
+        ban.ban_type = params[:ban_type]
+      end
+      if params[:ban_description]
+        ban.ban_description = params[:ban_description]
+      end
+      if params[:ban_url]
+        ban.ban_url = params[:ban_url]
+      end
+      ban.save
+      bans.push(ban)
+    }
+    render json: {
+      status: 200,
+      bans: bans,
+    }
+  end
+
   def get_country_banner
     country = Country.find_by_code( params[:bannee])
     bans = Ban.where( bannee: country)
@@ -54,6 +85,29 @@ class Api::V1::BansController < Api::V1::BaseController
       ban: ban_json
     }
   end
+
+  def get_many_ban
+    banner = Country.find_by_code( params[:banner])
+    bannees_code = params[:bannees].split(',')
+    bans = []
+    bannees_code.each { |bannee_code|
+      bannee = Country.find_by_code( bannee_code )
+      ban = Ban.find_by( bannee: bannee, banner: banner)
+      next unless ban
+      ban_json = JSON.parse(ban.to_json)
+      banner_id = ban_json["banner_id"]
+      bannee_id = ban_json["bannee_id"]
+      banner_code = Country.find(banner_id).code
+      bannee_code = Country.find(bannee_id).code
+      ban_json["banner_code"] = banner_code
+      ban_json["bannee_code"] = bannee_code
+      bans.push(ban_json)
+    }
+    render json: {
+      bans: bans
+    }
+  end
+
 
   def delete_ban
     authenticate
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/ban' => 'bans#create'
+      post '/many_ban' => 'bans#create_many'
       get '/ban/:bannee' => 'bans#get_country_banner'
       get '/ban/:bannee/:banner' => 'bans#get_ban'
+      get '/many_ban/:banner' => 'bans#get_many_ban'
       delete '/ban/:bannee/:banner' => 'bans#delete_ban'
     end
   end
