2d03a0f85d43b6ffb4588d6045b6e9fa51fdba0b
klementtan <klementtan@gmail.com>
2020-03-19 14:21:33 +0800

    update db config 3

--- config/initializers/ar_innodb_row_format.rb
+++ config/initializers/ar_innodb_row_format.rb
@@ -1,14 +1,10 @@
-# ActiveSupport.on_load :active_record do
-#   module ActiveRecord::ConnectionAdapters
-#     class AbstractMysqlAdapter
-#       def create_table_with_innodb_row_format(table_name, options = {})
-#         table_options = options.reverse_merge(:options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC')
+# config/initializers/mysqlpls.rb
+require 'active_record/connection_adapters/abstract_mysql_adapter'
 
-#         create_table_without_innodb_row_format(table_name, table_options) do |td|
-#          yield td if block_given?
-#         end
-#       end
-#       alias_method_chain :create_table, :innodb_row_format
-#     end
-#   end
-# end
+module ActiveRecord
+  module ConnectionAdapters
+    class AbstractMysqlAdapter
+      NATIVE_DATABASE_TYPES[:string] = { :name => "varchar", :limit => 191 }
+    end
+  end
+end
