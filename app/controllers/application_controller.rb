4e2d1c8642bfeb59db0eac7dbf72778089bf2c29
klementtan <klementtan@gmail.com>
2020-05-28 01:44:59 +0800

    Add health check route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,20 @@
-class ApplicationController < ActionController::API; end
+class ApplicationController < ActionController::API;
+  def health_check
+    database_connection = false
+    require './config/environment.rb' # Assuming the script is located in the root of the rails app
+    begin
+      ActiveRecord::Base.establish_connection # Establishes connection
+      puts 'ActiveRecord::Base.establish_connection Pass!'
+      ActiveRecord::Base.connection # Calls connection object
+      puts 'ActiveRecord::Base.connection pass'
+      database_connection = true if ActiveRecord::Base.connected?
+      puts 'ActiveRecord::Base.connected? pass'
+    rescue
+      puts 'NOT CONNECTED!'
+    end
+    render json: {
+        sever_running: true,
+        database_connection: database_connection
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get '/helloworld' => 'users#helloworld'
+  get '/' => 'application#health_check'
   namespace :api do
     namespace :v1 do
+      get '/helloworld' => 'users#helloworld'
 
       post '/ban' => 'bans#create'
       post '/many_ban' => 'bans#create_many'
