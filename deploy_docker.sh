243f583b12fdedd7da9ae604909523eea4547bdc
klementtan <klementtan@gmail.com>
2020-05-28 02:00:59 +0800

    Add deploy script

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,5 +1,4 @@
 docker build -t travelnogo .
 docker tag travelnogo:latest gcr.io/$PROJECT/travelnogo
 docker push gcr.io/$PROJECT/travelnogo
-kubectl rolling-update -f deployment.yml --image=gcr.io/travelnogo/travelnogo:latest
 
