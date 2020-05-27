fe893f74751e3ec127342c45fc2bd2a25eef1db3
klementtan <klementtan@gmail.com>
2020-05-28 02:10:27 +0800

    Update db host

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,4 +1,4 @@
 docker build -t travelnogo .
-docker tag travelnogo:latest gcr.io/$PROJECT/travelnogo
-docker push gcr.io/$PROJECT/travelnogo
+docker tag travelnogo:latest gcr.io/travelnogo/travelnogo
+docker push gcr.io/travelnogo/travelnogo
 
--- deploy_script.sh
+++
@@ -1,2 +0,0 @@
-docker build -t klementtandn/travelnogo-api_web:latest .
-docker push klementtandn/travelnogo-api_web:latest
