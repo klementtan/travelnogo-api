32ac8a5ec5e5089e0e5da5079f57c036ce5fac63
klementtan <klementtan@gmail.com>
2020-05-28 00:50:51 +0800

    Add db debug

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,8 +8,11 @@ class Api::V1::UsersController < Api::V1::BaseController
     require './config/environment.rb' # Assuming the script is located in the root of the rails app
     begin
       ActiveRecord::Base.establish_connection # Establishes connection
+      puts 'ActiveRecord::Base.establish_connection Pass!'
       ActiveRecord::Base.connection # Calls connection object
+      puts 'ActiveRecord::Base.connection pass'
       database_connection = true if ActiveRecord::Base.connected?
+      puts 'ActiveRecord::Base.connected? pass'
     rescue
       puts 'NOT CONNECTED!'
     end
