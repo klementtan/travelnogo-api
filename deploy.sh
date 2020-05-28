e438222207414a8b22fbd5b8b1ebf1764ad3151e
klementtan <klementtan@gmail.com>
2020-05-28 22:07:05 +0800

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 docker build -t travelnogo .
 docker tag travelnogo:latest gcr.io/travelnogo/travelnogo
 docker push gcr.io/travelnogo/travelnogo
+kubectl delete -f deployment.yml
+kubectl delete -f redis-master.yml
+kubectl apply -f deployment.yml
+kubectl apply -f service.yml
 
