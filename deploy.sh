278cdf9a5083cdef34e34ac977132adda5648d15
klementtan <klementtan@gmail.com>
2020-05-30 01:11:47 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 docker build -t travelnogo .
 docker tag travelnogo:latest gcr.io/travelnogo/travelnogo
 docker push gcr.io/travelnogo/travelnogo
-kubectl delete -f deployment.yml
-kubectl delete -f redis-master.yml
-kubectl apply -f deployment.yml
-kubectl apply -f redis-master.yml
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 
