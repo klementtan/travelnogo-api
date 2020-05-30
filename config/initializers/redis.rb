9dca7b19a1e4c619bcfd4fb47a4e1e46f9054e99
klementtan <klementtan@gmail.com>
2020-05-30 22:06:26 +0800

    Add redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,14 +1,5 @@
+REDIS = Redis.new(url: ENV['REDIS_URL'])
 
-redis_host = ENV['REDIS_HOST'] || 'localhost'
-redis_port = ENV['REDIS_PORT'] || 6379
-
-# The constant below will represent ONE connection, present globally in models, controllers, views etc for the instance. No need to do Redis.new everytime
-REDIS = nil
-if ENV['production']
-  REDIS  = Redis.new(url: ENV['REDIS_URL'])
-else
-  REDIS = Redis.new(host: redis_host, port: redis_port.to_i)
-end
 
 
 FirebaseIdToken.configure do |config|
