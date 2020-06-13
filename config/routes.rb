83fb2ec4f3bb9d0ccc095acab2e82ee2707a9ed1
klementtan <klementtan@gmail.com>
2020-06-13 19:14:42 +0800

    Remove auth

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
       get '/user/' => 'users#get_user'
       get '/user/check_valid_email' => 'users#check_valid_email'
       post '/user/firebase' => 'users#update_user_uuid'
-
+    
       get '/utils/maintenance_check' => 'utils#maintenance_check'
       post '/utils/maintenance_check_and_resolve' => 'utils#maintenance_check_and_resolve'
       get '/utils/health_check' => 'utils#health_check'
