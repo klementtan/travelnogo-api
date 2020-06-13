2fbc2367cbe86b8d65b053cac91a482221159ca3
klementtan <klementtan@gmail.com>
2020-06-13 13:26:22 +0800

    Add rails env check"

--- script/start_web.sh
+++ script/start_web.sh
@@ -3,7 +3,7 @@
 if [[ -a /tmp/puma.pid ]]; then
   rm /tmp/puma.pid
 fi
-
+echo $RAILS_ENV
 if [[ $RAILS_ENV == "production" ]]; then
   echo "in prod"
   EDITOR="mate --wait" bundle exec rails credentials:edit
